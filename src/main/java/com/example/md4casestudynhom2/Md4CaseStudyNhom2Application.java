b386b8468479cb2facaaee5597beab9351664e2c
Nguyenphangiang <phancruel@gmail.com>
2022-05-10 20:36:13 +0700

    create package, ignore envfile

---
+++ src/main/java/com/example/md4casestudynhom2/Md4CaseStudyNhom2Application.java
@@ -0,0 +1,13 @@
+package com.example.md4casestudynhom2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md4CaseStudyNhom2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md4CaseStudyNhom2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/md4casestudynhom2/Md4CaseStudyNhom2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.md4casestudynhom2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md4CaseStudyNhom2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
